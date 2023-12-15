b684624dac0d4967554117f1d6587ee7387efd65
stanleywanjau <stanleywanjau371@gmail.com>
2023-12-15 13:57:42 +0300

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES 
+('Mr. Chocolate',12,'F','black',NULL,true),
+('Rowdy',1,'M','red',NULL,false),
+('Tabitha',2,'F','white',NULL,true),
+('Sergeant Brown',30,'F','brown',NULL,false),
+('Melissa',5,'F','black',NULL,false),
+('Grinch',6,'F','white',NULL,false),
+('Wendy',7,'F','black',NULL,true),
+(NULL,NULL,'F','black',NULL,true);
